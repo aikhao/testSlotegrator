366133149cedf85003c6edc105bbc69eba940fd2
Oleg <oleg@x-cart.com>
2022-10-06 18:38:20 +0400

    refactor ui tests

--- src/main/java/com/testslotegrator/Steps/Player.java
+++ src/main/java/com/testslotegrator/Steps/Player.java
@@ -1,15 +1,21 @@
 package com.testslotegrator.Steps;
 
+import com.testslotegrator.helper.Constants;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
+import static com.testslotegrator.properties.Properties.urlFront;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.greaterThan;
 
 public class Player {
@@ -29,6 +35,31 @@ public class Player {
 
     @Given("Navigate to Page Players")
     public void navigateToPagePlayers() {
+        driver.get(urlFront + Constants.Urls.player);
 
+    }
+
+    @Then("A specify sorting status as \"([^\"]*)\"$")
+    public void aSpecifySortingStatusAsNotActive(String value) {
+        new Select(driver.findElement(By.name("PlayerSearch[status]"))).selectByVisibleText(value);
+
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.elementToBeClickable(By.cssSelector("#payment-system-transaction-grid:not(.grid-view-loading)")));
+    }
+
+    @Then("Content table have \"([^\"]*)\" count")
+    public void contentTableHaveCount(int arg0) {
+//        assertThat(driver.findElements(By.cssSelector("div.page table tbody tr")).size(), greaterThan(arg0));
+    }
+
+    @Then("All line in table have status \"([^\"]*)\"$")
+    public void allLineInTableHaveStatus(String arg0) {
+        List<WebElement> elements = driver.findElements(By.cssSelector("div.page table tbody tr"));
+        for (WebElement element:elements) {
+            assertThat(
+                    element.findElement(By.cssSelector("td:nth-child(15) span")).getText(),
+                    containsString(arg0)
+            );
+        }
     }
 }
--- src/test/java/com/testslotegrator/web/features/test.feature
+++ src/test/java/com/testslotegrator/web/features/test.feature
@@ -7,7 +7,7 @@ Feature: test
     And A User clicks on Submit button
     Then Application open Dashboard page
 
- Scenario: A valid email id is allowed
+ Scenario: A success authorization and open Player page
     Given Navigate to Page Login
     When A User enters a valid login
     And A User enters a valid password
@@ -16,7 +16,17 @@ Feature: test
     Given Navigate to Page Players
     Then Application open Players page
 
-
+   Scenario: A success authorization and change sorting on player page
+      Given Navigate to Page Login
+      When A User enters a valid login
+      And A User enters a valid password
+      When A User clicks on Submit button
+      Then Application open Dashboard page
+      Given Navigate to Page Players
+      Then Application open Players page
+      And A specify sorting status as "Not active"
+#      Then Content table have "1" count
+      Then All line in table have status "Not active"
 
 
 
